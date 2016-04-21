2c17649f37bb84417fdd555b79301de07ebc8d24
Igor <I.Matvievski@uarank.com>
2016-04-21 11:25:30 +0300

    add photoswipe rendering

--- js/MobileView.js
+++ js/MobileView.js
@@ -8,7 +8,24 @@ function reactRender() {
 			return { data: [] };
 		},
 		componentDidMount: function componentDidMount() {
+			/*$.ajax({
+   	url: "/Constructor/GetPage",
+   	type: "POST",
+   	cache: false,
+   	data: {
+   		id: $("#ProjectId").val(),
+   		versionId: $("#versionId").val(),
+   		pageId: $("#pageId").val()
+   	},
+   	success: function(json) {
+   		this.setState({data: json.Rows});
+   	}.bind(this),
+   	error: function(xhr, status, err) {
+   		console.error(status, err.toString());
+   	}.bind(this)
+   });*/
 			this.setState({ data: applicationData.Pages[indexPage].Rows });
+			setTimeout('initPhotoSwipeFromDOM(".my-gallery")', 1000);
 		},
 		render: function render() {
 			var rowModels = this.state.data.map(function (row) {
@@ -37,6 +54,45 @@ function reactRender() {
 		}
 	});
 
+	var GalleryContainer = React.createClass({
+		displayName: "GalleryContainer",
+
+		render: function render() {
+			return React.createElement(
+				"div",
+				{ className: "my-gallery" },
+				this.createItems(this.props.data)
+			);
+		},
+		createItems: function createItems(items) {
+			var output = [];
+			for (var i = 0; i < items.length; i++) {
+				if (i == 0) {
+					output.push(React.createElement(
+						"figure",
+						{ key: items[i].Id },
+						React.createElement(
+							"a",
+							{ href: items[i].Link, itemprop: "contentUrl", "data-size": "964x1024" },
+							React.createElement("img", { src: items[i].Link, alt: "Image description" })
+						)
+					));
+				} else {
+					output.push(React.createElement(
+						"figure",
+						{ className: "hidden", key: items[i].Id },
+						React.createElement(
+							"a",
+							{ href: items[i].Link, itemprop: "contentUrl", "data-size": "964x1024" },
+							React.createElement("img", { src: items[i].Link, alt: "Image description" })
+						)
+					));
+				}
+			}
+			return output;
+		}
+	});
+
 	var CellContent = React.createClass({
 		displayName: "CellContent",
 
@@ -61,6 +117,19 @@ function reactRender() {
 			}
 			if (data.ContentTypeId == 1) {
 				return React.createElement("div", { className: "cell-container col-xs-" + data.Colspan + " col-sm-" + data.Colspan + " col-md-" + data.Colspan + " col-lg-" + data.Colspan, dangerouslySetInnerHTML: { __html: data.Value } });
+			}
+			if (data.ContentTypeId == 8) {
+				return React.createElement(
+					"div",
+					{ className: "cell-container col-xs-" + data.Colspan + " col-sm-" + data.Colspan + " col-md-" + data.Colspan + " col-lg-" + data.Colspan, onClick: this.onClickCell },
+					React.createElement(GalleryContainer, { data: data.Resourceses })
+				);
+			}
+			if (data.ContentTypeId == 7) {
+				return React.createElement("div", { className: "cell-container col-xs-" + data.Colspan + " col-sm-" + data.Colspan + " col-md-" + data.Colspan + " col-lg-" + data.Colspan, onClick: this.onClickCell, dangerouslySetInnerHTML: { __html: data.Value } });
+			}
+			if (data.ContentTypeId == 9) {
+				return React.createElement("div", { className: "cell-container col-xs-" + data.Colspan + " col-sm-" + data.Colspan + " col-md-" + data.Colspan + " col-lg-" + data.Colspan, onClick: this.onClickCell, dangerouslySetInnerHTML: { __html: data.Value } });
 			}
 		}
 	});
