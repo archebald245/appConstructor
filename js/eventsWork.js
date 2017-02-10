a5ed5568ac1f6f843cc8719cba21e71f962c699b
user-2495\user <A.Sashuk@newline.tech>
2017-02-10 16:44:14 +0200

    add scroll top after go to page event

--- js/eventsWork.js
+++ js/eventsWork.js
@@ -3,7 +3,7 @@ function goToPage(index) {
     showActivePageInMenu(index);
     $("#container").empty();
     slideUp();
-
+    $("html, body").animate({ scrollTop: -$(document).height() }, "fast");
     reactRender();
 
     $("a:not(.galleryHref)").each(function(i, e){
