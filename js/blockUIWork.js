2202e8067069e8f0a2b433df999bc12c708dcc16
yevhen pugachov <y.pugachov@newline.tech>
2017-03-31 13:20:58 +0300

    fix go to page

--- js/blockUIWork.js
+++ js/blockUIWork.js
@@ -25,14 +25,14 @@ function unBlockUi() {
     if (pageWithGeneralBg.length > 0) {
         pageStyles = pageWithGeneralBg[0].Style;
     }
-    if(applicationData.IsTrackingLastPage){
-    var idLastPage = getLastOpenPage();
-    var lastPage = applicationData.Pages.filter(function(p){return p.Id = idLastPage})[0]
-    if (lastPage.BackgroundImagePath != null) {
-        pageStyles = lastPage.Style;
-    }
-    }else{
-        var StartPage = applicationData.Pages.filter(function(p){return p.IsStartPage})[0];
+    if (applicationData.IsTrackingLastPage) {
+        var idLastPage = getLastOpenPage();
+        var lastPage = applicationData.Pages.filter(function(p) { return p.Id == idLastPage })[0]
+        if (lastPage.BackgroundImagePath != null) {
+            pageStyles = lastPage.Style;
+        }
+    } else {
+        var StartPage = applicationData.Pages.filter(function(p) { return p.IsStartPage })[0];
         if (StartPage.BackgroundImagePath != null) {
             pageStyles = StartPage.Style;
         }
