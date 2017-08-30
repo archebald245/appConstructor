60dbaedfaf34809c768a1969e0cbfd39f0a5b7d8
yevhen pugachov <y.pugachov@newline.tech>
2017-08-30 18:12:52 +0300

    fix bugs

--- js/initWork.js
+++ js/initWork.js
@@ -259,13 +259,13 @@ function checkConnection() {
 }
 
 function sendPushNotificationToken() {
-    if ($.jStorage.get('notificationToken') == null) {
-        var token = $.jStorage.get('notificationToken');
-        var projectId = applicationData.ProjectId;
+    if ($.jStorage.get('notificationToken') !== null) {
+        var tokenToSend = $.jStorage.get('notificationToken');
+        var projectIdToSend = applicationData.ProjectId;
         $.ajax({
             type: "POST",
             url: applicationData.UrlForUpdateApp + "/PushNotification/SaveUserToken",
-            data: { token: token, projectId: projectId },
+            data: { token: tokenToSend, projectId: projectIdToSend },
             cache: false,
             success: function(response) {
                 $.jStorage.set('notificationTokenSuccess', response);
