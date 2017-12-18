d812867a18042c356bc1a5af9968883a1afd8a3c
yevhen pugachov <y.pugachov@newline.tech>
2017-12-18 20:21:48 +0200

    update appData, blockui

--- js/blockUIWork.js
+++ js/blockUIWork.js
@@ -1,15 +1,19 @@
 function blockUi() {
     $.blockUI({
-        css: {
-            border: 'none',
-            padding: '15px',
-            backgroundColor: '#000',
-            '-webkit-border-radius': '10px',
-            '-moz-border-radius': '10px',
-            opacity: .5,
-            color: '#fff'
-        }
+        message: $('#block-ui-container')
     });
+
+    // $.blockUI({
+    //     css: {
+    //         border: 'none',
+    //         padding: '15px',
+    //         backgroundColor: '#000',
+    //         '-webkit-border-radius': '10px',
+    //         '-moz-border-radius': '10px',
+    //         opacity: .5,
+    //         color: '#fff'
+    //     }
+    // });
 }
 
 
